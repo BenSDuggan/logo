badf6043d026cd51e60ec972041adbf5c8302d2d
BenSDuggan <dugganbens@gmail.com>
2021-03-08 16:15:15 -0500

    adding initial code to make logo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# logo
+# Logo
+
+Code to make my logo
+
+## Run
+
+To make the logo
+
+1. Run `make_xml.py`.  This will use an image called "logo.png".
+2. Run `make`.
+
---
+++ make_xml.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Variables
+logo_path = 'logo.png'
+
+# Load image
+img = plt.imread(logo_path)
+
+print(img)
+
+#plt.imshow(img)
+
+
+
+plt.show()
+
+
